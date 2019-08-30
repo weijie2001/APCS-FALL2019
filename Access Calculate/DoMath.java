c698f15fce5279c1614e5e3d937d21b943ee3d97
APCS3rd <APCS3rd@MHS-LT9-19>
2019-08-30 10:39:14 -0700

    Part 1 -- 1/2 done

---
+++ Access Calculate/DoMath.java
@@ -0,0 +1,12 @@
+//Period 3 Weijie OU 8/29/2019
+//Liberary of math methods
+
+
+public class DoMath {
+	
+	public static int () {
+		System.out.println("Calculate.square(2)");
+
+	}
+
+}
---
+++ Calculate Liberty/Calculate.java
@@ -0,0 +1,11 @@
+//Weijie Ou
+//8/29/2019
+//Liberary of math methods
+
+
+public class Calculate {
+	
+	public static int square(int number) {
+		int
+	}
+}
