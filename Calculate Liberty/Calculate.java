aa63805a2a70b3d5114602cc4ddb9f2a9cc28d0f
APCS3rd <APCS3rd@MHS-LT9-19>
2019-09-03 11:47:24 -0700

    Part 1

--- Calculate Liberty/Calculate.java
+++ Calculate Liberty/Calculate.java
@@ -6,6 +6,33 @@
 public class Calculate {
 	
 	public static int square(int number) {
-		int
+		int answer;
+		answer=number*number;
+		return answer;
 	}
-}
+	public static int cube(int number) {
+		int answer;
+		answer=number*number*number;
+		return answer;
+	}
+	public static double average(double number1, double number2) {
+		double answer=(number1 + number2)/2;
+		return answer;
+	}
+	public static double average(double number1, double number2, double number3) {
+		double answer;
+		answer=(number1 + number2 + number3)/3;
+		return answer;
+	}
+	public static double toDegrees(double number) {
+		double answer;
+		answer=(number * 180/3.14159);
+		return answer;
+	}
+	
+	
+		
+		
+	}
+		
+	}
